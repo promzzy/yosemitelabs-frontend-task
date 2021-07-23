4eb9693bea31f91bb67039814cd23f09f7eb64c1
promzzy <promisestephen29@gmail.com>
2021-07-23 14:19:26 +0100

    created context, actions and mapped out pokemons from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
+import React from 'react'
+import { PokemonProvider } from './context/PokemonContext';
+import { ChakraProvider } from "@chakra-ui/react"
+import PokemonsList from './components/PokamonsList';
 function App() {
   return (
+    <ChakraProvider>
+        <PokemonProvider>
     <div>
-
+      <PokemonsList />
+      {/* <Pokedex /> */}
     </div>
+    {/* <div className='form-wrapper'>
+      <PokemonForm />
+    </div> */}
+  </PokemonProvider>
+    </ChakraProvider>
   );
 }
 
